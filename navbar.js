8bfc41830de7c8531afbec355b8cf0852f49c7ed
Ahmad Zaki <shaikhzaki8@gmail.com>
2022-10-13 13:50:40 +0530

    .

---
+++ navbar.js
@@ -0,0 +1,15 @@
+function navbar() {
+  return `<div id="container">
+    <p class="navText">
+        <a class="anchorNav" href="register.html">Registration Page</a>
+    </p>
+    <p class="navText">
+        <a class="anchorNav" href="dashboard.html">Dashboard Page</a>
+    </p>
+    <p class="navText">
+        <a class="anchorNav" href="vaccinated.html">Vaccination Page</a>
+    </p>
+</div>`;
+}
+
+export default navbar;
